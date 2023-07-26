b5e46408b37741fe75cde36fd3394bcef10efad8
Ali Durul <80897590+AliDurul@users.noreply.github.com>
2023-07-26 22:10:34 +0200

    Indecision app moved to this repo from another

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import IndecisionApp from "./Compenents/IndecisionApp";
+
+
+// import Train from "./Train";
+// import Playgorund from "./Playgorund";
+
+function App() {
+  return (
+    <div>
+      <IndecisionApp />
+    </div>
+  );
+}
+
+export default App;
